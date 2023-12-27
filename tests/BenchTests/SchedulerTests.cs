18b8f9a7b7e55efffd1dbbc57c68ede2c54638c4
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2023-12-27 17:59:47 +0000

    Scheduler unit tests

--- tests/BenchTests/SchedulerTests.cs
+++ tests/BenchTests/SchedulerTests.cs
@@ -62,11 +62,9 @@ public class SchedulerTests
     }
 
     [TestMethod]
-    public async Task Scheduler_OneNormalProcess()
+    public async Task OneNormalProcess()
     {
-        var emulator = new Emulator();
-
-        var (_, snapshot) = await X16TestHelper.EmulateTemplateChanges(@"
+        var (emulator, snapshot) = await X16TestHelper.EmulateTemplateChanges(@"
                 import Globals=""..\..\..\..\..\src\bench\globals.bmasm"";
                 import Scheduler=""..\..\..\..\..\src\bench\scheduler.bmasm"";
                 import Init=""..\..\..\..\..\src\bench\initialisation.bmasm"";
@@ -107,7 +105,7 @@ public class SchedulerTests
 
                 ; create procs
                 Init.Setup();                
-                ", emulator);
+                ");
 
         snapshot.Snap();
 
@@ -119,6 +117,69 @@ public class SchedulerTests
             .Is(MemoryAreas.BankedRam, 0x01a301, 0x01) // current process
             .Is(MemoryAreas.BankedRam, 0x01a302, 0x02) // index to look from next time
             .Is(MemoryAreas.BankedRam, 0x01a303, 0x0e) // number of processes to check before looping
+            .CanChange(Registers.Y)
+            .CanChange(Registers.X)
+            .IgnoreNumericCpuFlags()
+            .IgnoreVia()
+            .IgnoreVera()
+            .IgnoreStackHistory()
+            .AssertNoOtherChanges();    
+    }
+    
+    [TestMethod]
+    public async Task OneNormalProcess_AllDone()
+    {
+        var (emulator, snapshot) = await X16TestHelper.EmulateTemplateChanges(@"
+                import Globals=""..\..\..\..\..\src\bench\globals.bmasm"";
+                import Scheduler=""..\..\..\..\..\src\bench\scheduler.bmasm"";
+                import Init=""..\..\..\..\..\src\bench\initialisation.bmasm"";
+                import Memory=""..\..\..\..\..\src\bench\memory.bmasm"";
+                import Process=""..\..\..\..\..\src\bench\process.bmasm"";
+                .machine CommanderX16R42
+
+                .org $810
+
+                cld
+                sei
+
+                ldx #$ff
+                txs
+
+                lda #1
+                sta RAM_BANK
+                sta IEN
+                stz CTRL
+
+                jsr bitbench_memory:initialise
+                jsr bitbench_scheduler:initialise
+                jsr bitbench_process:initialise
+
+                lda #02 ; main bank is #2
+                jsr bitbench_process:createprocess
+
+                ldy #01
+                jsr bitbench_scheduler:enable_process
+
+                jsr bitbench_scheduler:reset
+                jsr bitbench_scheduler:get_next_process
+                
+                stp
+
+                jsr bitbench_scheduler:get_next_process
+
+                stp
+
+                ; create procs
+                Init.Setup();                
+                ");
+
+        snapshot.Snap();
+
+        emulator.Emulate();
+
+        snapshot.Compare()
+            .Is(Registers.A, 0x00) // Nothing else to do
+            .Is(MemoryAreas.BankedRam, 0x01a302, 0x00) // index to look from next time
             .CanChange(Registers.Y)
             .CanChange(Registers.X)
             .IgnoreNumericCpuFlags()
